c53ab98f682781ee8af60d633d1c80141f990834
Dmitrii Derendyaev <dds_stalker@mail.ru>
2025-03-14 19:57:11 +0400

    feature: сделать тестовый коммит

--- src/main/java/ru/derendyaev/CICD/TestController.java
+++ src/main/java/ru/derendyaev/CICD/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Test CICD process";
+        return "Test CICD completed GitHub actions";
     }
 }
