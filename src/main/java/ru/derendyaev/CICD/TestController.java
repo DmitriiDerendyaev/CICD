c9621ae7b5e052d0c745b05e66921f3c7a1e6677
Dmitrii Derendyaev <dds_stalker@mail.ru>
2025-03-14 20:34:08 +0400

    feature: Switch to SSH key authentication

--- src/main/java/ru/derendyaev/CICD/TestController.java
+++ src/main/java/ru/derendyaev/CICD/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Test CICD completed GitHub actions";
+        return "Switch to SSH key authentication";
     }
 }
